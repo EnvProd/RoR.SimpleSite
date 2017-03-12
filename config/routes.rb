84950cf6b1050ee073e94216656ff85e91266397
Chernyaev Artyom <leo240532@yandex.com>
2017-03-12 11:26:11 +0300

    trash

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   root 'application#hello'
   resources :users
 
-  # Прогнозирую все необходимые страницы
-  # get '/admin/users' - Админка со списком всех пользователей
-  # get '/admin/'
-
   # Помощь по Routes 
   # http://rusrails.ru/rails-routing
 end
